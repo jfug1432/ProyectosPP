43bac675bbe4b84d54df6969c43946a703c44147
jfug <francisco.urias@uadeo.mx>
2022-01-28 09:37:48 -0700

    este dia creamos el promedio de 3 calificaciones

--- PROYECTOSPOO/promediodetrescalificaciones.Designer.cs
+++ PROYECTOSPOO/promediodetrescalificaciones.Designer.cs
@@ -29,31 +29,19 @@ namespace ProyectosPP.PROYECTOSPOO
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(205, 59);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 0;
             // 
             // promediodetrescalificaciones
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.textBox1);
             this.Name = "promediodetrescalificaciones";
             this.Text = "promediodetrescalificaciones";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TextBox textBox1;
     }
 }
