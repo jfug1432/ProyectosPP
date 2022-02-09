8e3bae146b88a0c2fbd8ba8a1cfac5de40b3e102
jfug <francisco.urias@uadeo.mx>
2022-02-09 10:02:47 -0700

    porcentaje

---
+++ CLASES/CLCALCULADORA.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectosPP.CLASES
+{
+    class CLCALCULADORA
+    {
+        // Campos
+        private double valor1;
+        private double valor2;
+
+        public CLCALCULADORA(double valor1, double valor2)
+        {
+            this.valor1 = valor1;
+            this.valor2 = valor2;
+        }
+        public double suma()
+        {
+            return (valor1 + valor2);
+        }
+        public double resta()
+        {
+            return (valor1 - valor2);
+        }
+        public double multiplica()
+        {
+            return (valor1 * valor2);
+        }
+        public double divide()
+        {
+            return (valor1 / valor2);
+        }
+        public double raiz(double valor3)
+        {
+            return (Math.Sqrt(valor3));
+        }
+        public double porcentaje()
+        {
+            return (valor2 * (valor1 / 100));
+        }
+    }
+}
--- PROYECTOSPOO/calculadora.Designer.cs
+++ PROYECTOSPOO/calculadora.Designer.cs
@@ -29,7 +29,7 @@ namespace ProyectosPP.PROYECTOSPOO
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtvalores = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -51,16 +51,17 @@ namespace ProyectosPP.PROYECTOSPOO
             this.button19 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtvalores
             // 
-            this.textBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.ForeColor = System.Drawing.SystemColors.Window;
-            this.textBox1.Location = new System.Drawing.Point(30, 59);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(259, 34);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            this.txtvalores.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtvalores.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtvalores.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtvalores.Location = new System.Drawing.Point(30, 59);
+            this.txtvalores.Name = "txtvalores";
+            this.txtvalores.ReadOnly = true;
+            this.txtvalores.Size = new System.Drawing.Size(259, 29);
+            this.txtvalores.TabIndex = 0;
+            this.txtvalores.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // button1
             // 
@@ -71,6 +72,7 @@ namespace ProyectosPP.PROYECTOSPOO
             this.button1.TabIndex = 1;
             this.button1.Text = "C";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -81,6 +83,7 @@ namespace ProyectosPP.PROYECTOSPOO
             this.button2.TabIndex = 2;
             this.button2.Text = "+";
             this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -91,6 +94,7 @@ namespace ProyectosPP.PROYECTOSPOO
             this.button3.TabIndex = 3;
             this.button3.Text = "-";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
@@ -101,6 +105,7 @@ namespace ProyectosPP.PROYECTOSPOO
             this.button4.TabIndex = 4;
             this.button4.Text = "*";
             this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -111,6 +116,7 @@ namespace ProyectosPP.PROYECTOSPOO
             this.button5.TabIndex = 5;
             this.button5.Text = "/";
             this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -120,6 +126,7 @@ namespace ProyectosPP.PROYECTOSPOO
             this.button6.TabIndex = 6;
             this.button6.Text = "7";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button7
             // 
@@ -129,6 +136,7 @@ namespace ProyectosPP.PROYECTOSPOO
             this.button7.TabIndex = 7;
             this.button7.Text = "8";
             this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
@@ -138,6 +146,7 @@ namespace ProyectosPP.PROYECTOSPOO
             this.button8.TabIndex = 8;
             this.button8.Text = "9";
             this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // button9
             // 
@@ -147,6 +156,7 @@ namespace ProyectosPP.PROYECTOSPOO
             this.button9.TabIndex = 11;
             this.button9.Text = "6";
             this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button10
             // 
@@ -156,6 +166,7 @@ namespace ProyectosPP.PROYECTOSPOO
             this.button10.TabIndex = 10;
             this.button10.Text = "5";
             this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
             // 
             // button11
             // 
@@ -165,6 +176,7 @@ namespace ProyectosPP.PROYECTOSPOO
             this.button11.TabIndex = 9;
             this.button11.Text = "4";
             this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
             // 
             // button12
             // 
@@ -174,6 +186,7 @@ namespace ProyectosPP.PROYECTOSPOO
             this.button12.TabIndex = 14;
             this.button12.Text = "3";
             this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
             // 
             // button13
             // 
@@ -183,6 +196,7 @@ namespace ProyectosPP.PROYECTOSPOO
             this.button13.TabIndex = 13;
             this.button13.Text = "2";
             this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
             // 
             // button14
             // 
@@ -192,6 +206,7 @@ namespace ProyectosPP.PROYECTOSPOO
             this.button14.TabIndex = 12;
             this.button14.Text = "1";
             this.button14.UseVisualStyleBackColor = true;
+            this.button14.Click += new System.EventHandler(this.button14_Click);
             // 
             // button15
             // 
@@ -201,6 +216,7 @@ namespace ProyectosPP.PROYECTOSPOO
             this.button15.TabIndex = 15;
             this.button15.Text = "0";
             this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
             // 
             // button16
             // 
@@ -210,6 +226,7 @@ namespace ProyectosPP.PROYECTOSPOO
             this.button16.TabIndex = 16;
             this.button16.Text = ".";
             this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
             // 
             // button17
             // 
@@ -220,6 +237,7 @@ namespace ProyectosPP.PROYECTOSPOO
             this.button17.TabIndex = 17;
             this.button17.Text = "%";
             this.button17.UseVisualStyleBackColor = false;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
             // 
             // button18
             // 
@@ -230,6 +248,7 @@ namespace ProyectosPP.PROYECTOSPOO
             this.button18.TabIndex = 18;
             this.button18.Text = " √ ";
             this.button18.UseVisualStyleBackColor = false;
+            this.button18.Click += new System.EventHandler(this.button18_Click);
             // 
             // button19
             // 
@@ -240,10 +259,11 @@ namespace ProyectosPP.PROYECTOSPOO
             this.button19.TabIndex = 19;
             this.button19.Text = "=";
             this.button19.UseVisualStyleBackColor = false;
+            this.button19.Click += new System.EventHandler(this.button19_Click);
             // 
             // calculadora
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(325, 483);
@@ -266,7 +286,7 @@ namespace ProyectosPP.PROYECTOSPOO
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtvalores);
             this.Font = new System.Drawing.Font("Lucida Sans Unicode", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "calculadora";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -279,7 +299,7 @@ namespace ProyectosPP.PROYECTOSPOO
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtvalores;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
